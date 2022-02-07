ec08f3a204852e2b6a1c9ac1c76375ad24395f6a
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-02-08 00:48:23 +0100

    Add comment

--- src/BF2Dashboard.Domain/Services/ServerListService.cs
+++ src/BF2Dashboard.Domain/Services/ServerListService.cs
@@ -11,6 +11,9 @@ public static class ServerListService
     {
         try
         {
+            // TODO I'M pretty sure there's a bug here caching this across multiple sessions.
+            // TODO Try fixing with the DI refactoring by setting a scope per session (main goal is to prevent F5 bombs from querying the API)
+            // TODO investigate maybe there's a cache time I could set somehow. So that it gets cached for 20s but then expires and gets fetched again
             if (CachedServerList == null)
                 CachedServerList = await ServerListRepository.QueryServerList();
 
