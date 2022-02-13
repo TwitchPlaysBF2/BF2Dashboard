75f9660a53b7c9e22c9dd66d520756cc19cd6f50
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-02-13 20:24:52 +0100

    Set up framework

--- src/BF2Dashboard.UI/Program.cs
+++ src/BF2Dashboard.UI/Program.cs
@@ -1,5 +1,7 @@
 using BF2Dashboard.UI;
 using Blazored.LocalStorage;
+using Havit.Blazor.Components.Web;
+using Havit.Blazor.Components.Web.Bootstrap;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddHxServices();
 
 await builder.Build().RunAsync();
