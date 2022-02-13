6e7118f348782b03895490d764977a527fac757b
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-02-13 19:56:21 +0100

    Add a bunch of experimental attempts to render a lot of stuff but it was really laggy to press a button within the expandable UI component for some reason

--- src/BF2Dashboard.UI/Program.cs
+++ src/BF2Dashboard.UI/Program.cs
@@ -2,6 +2,7 @@ using BF2Dashboard.UI;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
