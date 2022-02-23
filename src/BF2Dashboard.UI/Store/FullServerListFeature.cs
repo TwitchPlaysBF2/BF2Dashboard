adfe7ea41240a2dcdb3fbe170648f962107fe6c0
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-02-23 23:54:58 +0100

    Rework Dashboard UI and introduce fluxor:
    - Introduce state management architecture following fluxor pattern
    - Separate dashboard without components for now (to avoid perf impact)
    - Use bootstrap accordion with bootstrap JS
    - Move browser storage dependency from UI to domain
    
    Thanks to Eric King for the excellent guidance:
    https://dev.to/mr_eking/advanced-blazor-state-management-using-fluxor-part-1-696

---
+++ src/BF2Dashboard.UI/Store/FullServerListFeature.cs
@@ -0,0 +1,10 @@
+﻿using Fluxor;
+
+namespace BF2Dashboard.UI.Store;
+
+public class FullServerListFeature : Feature<FullServerListState>
+{
+    public override string GetName() => nameof(FullServerListFeature);
+
+    protected override FullServerListState GetInitialState() => new(null);
+}
