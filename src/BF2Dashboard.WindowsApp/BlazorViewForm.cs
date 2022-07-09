fec889dacd92d96ca98782b6e88df19555bf9d4c
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-07-10 01:23:20 +0200

    Add experimental Blazor renderer form

---
+++ src/BF2Dashboard.WindowsApp/BlazorViewForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BF2Dashboard.WindowsApp.Infrastructure;
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BF2Dashboard.WindowsApp
+{
+    public partial class BlazorViewForm : Form
+    {
+        public BlazorViewForm()
+        {
+            InitializeComponent();
+            var services = new ServiceCollection();
+            services.AddWindowsFormsBlazorWebView();
+            blazorWebView1.HostPage = "wwwroot\\index.html";
+            blazorWebView1.Services = services.BuildServiceProvider();
+            blazorWebView1.RootComponents.Add<UI.Pages.Dashboard>("#app");
+        }
+
+        protected override void OnHandleCreated(EventArgs e) => DarkMode.Enable(Handle);
+    }
+}
