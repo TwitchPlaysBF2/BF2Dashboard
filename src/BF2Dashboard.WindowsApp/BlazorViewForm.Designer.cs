5717961ebe9751598016bec792c9040afae7e425
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-07-10 11:07:16 +0200

    Improve app style

--- src/BF2Dashboard.WindowsApp/BlazorViewForm.Designer.cs
+++ src/BF2Dashboard.WindowsApp/BlazorViewForm.Designer.cs
@@ -36,18 +36,20 @@
             this.blazorWebView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.blazorWebView1.Location = new System.Drawing.Point(0, 0);
             this.blazorWebView1.Name = "blazorWebView1";
-            this.blazorWebView1.Size = new System.Drawing.Size(800, 450);
+            this.blazorWebView1.Size = new System.Drawing.Size(1871, 1188);
             this.blazorWebView1.TabIndex = 0;
-            this.blazorWebView1.Text = "blazorWebView1";
+            this.blazorWebView1.Text = "If you see this, you might be missing the Evergreen WebView2 Runtime";
             // 
-            // Main
+            // BlazorViewForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1871, 1188);
             this.Controls.Add(this.blazorWebView1);
-            this.Name = "Main";
-            this.Text = "Main";
+            this.Icon = global::BF2Dashboard.WindowsApp.Properties.Resources.favicon;
+            this.Name = "BlazorViewForm";
+            this.Text = "BF2.TV App";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
         }
