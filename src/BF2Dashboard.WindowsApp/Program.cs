fd819b096ef62189dcac8d6d6ff19901b24d9e2c
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-07-10 11:08:27 +0200

    Change default app form to blazor view

--- src/BF2Dashboard.WindowsApp/Program.cs
+++ src/BF2Dashboard.WindowsApp/Program.cs
@@ -11,7 +11,7 @@ namespace BF2Dashboard.WindowsApp
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new LaunchForm());
+            Application.Run(new BlazorViewForm());
         }
     }
 }
