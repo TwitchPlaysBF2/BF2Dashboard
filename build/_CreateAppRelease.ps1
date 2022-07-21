875f2023e57420e42bd8e833b13f6abf32dbf7a5
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-07-22 01:58:41 +0200

    Raise app version for first release

--- build/_CreateAppRelease.ps1
+++ build/_CreateAppRelease.ps1
@@ -1,3 +1,11 @@
 cd ..\src\BF2TV.WindowsApp\
 dotnet publish /p:Configuration=Release /p:PublishProfile=Properties\PublishProfiles\PUBLISH
+Write-Output "*********************************************"
+Write-Output "*********************************************"
+Write-Output "*********************************************"
+Write-Output "*********************************************"
 Write-Output "*********** done creating release ***********"
+Write-Output "*********************************************"
+Write-Output "*********************************************"
+Write-Output "*********************************************"
+Write-Output "*********************************************"
