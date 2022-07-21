ec17c64202ccd115f7f3e615d6ef4e861a209595
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-07-21 23:58:37 +0200

    Provide interface for runtime detection

---
+++ src/BF2TV.BlazorWasm/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using BF2TV.Frontend.Infrastructure;
+
+namespace BF2TV.BlazorWasm.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static void RegisterBlazorWasmServices(this IServiceCollection services)
+    {
+        services.AddTransient<IEnvironment, BlazorWasmEnvironment>();
+    }
+}
---
+++ src/BF2TV.WindowsApp/Infrastructure/WinFormsEnvironment.cs
@@ -0,0 +1,9 @@
+﻿using BF2TV.Frontend.Infrastructure;
+
+namespace BF2TV.WindowsApp.Infrastructure;
+
+public class WinFormsEnvironment : IEnvironment
+{
+    public bool IsWeb() => false;
+    public bool IsApp() => !IsWeb();
+}
