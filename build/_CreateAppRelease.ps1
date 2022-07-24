c0d1441be6fae476b1af6ab913d9813aac911389
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-07-24 03:35:29 +0200

    Improve build script experience
    after successfully testing the auto-updating mechanism

--- build/_CreateAppRelease.ps1
+++ build/_CreateAppRelease.ps1
@@ -1,11 +1,22 @@
+Remove-Item 'bin/publish' -Recurse
+
 cd ..\src\BF2TV.WindowsApp\
 dotnet publish /p:Configuration=Release /p:PublishProfile=Properties\PublishProfiles\PUBLISH
 Write-Output "*********************************************"
 Write-Output "*********************************************"
 Write-Output "*********************************************"
 Write-Output "*********************************************"
+Write-Output "*********************************************"
 Write-Output "*********** done creating release ***********"
+Write-Output "********* press CTRL + F9 to build **********"
+Write-Output "******* the exectuable in inno setup ********"
 Write-Output "*********************************************"
 Write-Output "*********************************************"
 Write-Output "*********************************************"
 Write-Output "*********************************************"
+Write-Output "*********************************************"
+
+cd ../../build/bin
+
+Start .
+Start "C:\Program Files (x86)\Inno Setup 6\Compil32.exe"
