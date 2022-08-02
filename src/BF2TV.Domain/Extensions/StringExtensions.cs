ef831492ab02e424355b994039b227c264301a66
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-08-02 17:38:50 +0200

    Show "Join" button if SponsorText has Discord link
    Idea suggested by @cetteup

---
+++ src/BF2TV.Domain/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BF2TV.Domain.Extensions;
+
+public static class StringExtensions
+{
+    public static bool TryGetDiscordUrl(this string text, out string discordUrl)
+    {
+        var regex = new Regex(@"(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/.+[a-z]");
+        var match = regex.Match(text);
+        if (match.Success)
+        {
+            discordUrl = match.Value;
+            return true;
+        }
+
+        discordUrl = string.Empty;
+        return false;
+    }
+}
