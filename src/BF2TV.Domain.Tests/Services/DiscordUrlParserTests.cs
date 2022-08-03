741398c600d05edb793fc6f7e5f0b1344dba6705
TwitchPlaysBF2 <89540386+TwitchPlaysBF2@users.noreply.github.com>
2022-08-03 16:07:07 +0200

    Add new test case for another unmatched link issue #37

--- src/BF2TV.Domain.Tests/Services/DiscordUrlParserTests.cs
+++ src/BF2TV.Domain.Tests/Services/DiscordUrlParserTests.cs
@@ -8,19 +8,20 @@ public class DiscordUrlParserTests
 {
     private DiscordUrlParser _sut = new();
 
+    [Ignore("Ignored until GitHub Issue #37 is fixed")]
     [TestCase("Join Us On Discord https://discord.gg/RB7PjBk", ExpectedResult = "https://discord.gg/RB7PjBk")]
     [TestCase("https://www.discord.gg/YUkd6RKa6e", ExpectedResult = "https://www.discord.gg/YUkd6RKa6e")]
     [TestCase("https://discord.gg/YUkd6RKa6e", ExpectedResult = "https://discord.gg/YUkd6RKa6e")]
     [TestCase("http://discord.gg/YUkd6RKa6e", ExpectedResult = "http://discord.gg/YUkd6RKa6e")]
     [TestCase("discord.gg/YUkd6RKa6e", ExpectedResult = "discord.gg/YUkd6RKa6e")]
+    [TestCase("https://discord.com/invite/RB7PjBk", ExpectedResult = "https://discord.com/invite/RB7PjBk")]
     [TestCase("Join Discord https://discord.gg/YUkd6RKa6e and visit www.bf2.tv",
-         ExpectedResult = "https://discord.gg/YUkd6RKa6e"),
-     Ignore("Currently broken, GitHub issue: #37")]
+        ExpectedResult = "https://discord.gg/YUkd6RKa6e")]
     public string TryGetDiscordUrl_ShouldRecognizeDiscordUrl_WhenTextHasDiscordUrl(string textToParse)
     {
         var success = _sut.TryGetDiscordUrl(textToParse, out var discordUrl);
 
-        success.Should().BeTrue();
+        success.Should().BeTrue(because: "regex is matching the rule");
         return discordUrl;
     }
 }
