a05a3d340337ece2055698f799f52d55d928de68
cetteup <me@cetteup.com>
2025-07-24 22:53:05 +0200

    feat: pause periodic refresh while in the background

---
+++ src/BF2TV.Frontend/wwwroot/js/focusInterop.js
@@ -0,0 +1,12 @@
+﻿// wwwroot/focusInterop.js
+window.focusInterop = {
+    registerFocusHandlers: function (dotNetHelper) {
+        window.addEventListener("blur", () => {
+            dotNetHelper.invokeMethodAsync("OnWindowBlur");
+        });
+
+        window.addEventListener("focus", () => {
+            dotNetHelper.invokeMethodAsync("OnWindowFocus");
+        });
+    }
+};
